18431d6064d7611a990b0800cec53c235c2f3bf5
David-Pintado <david02pg@gmail.com>
2022-10-05 10:03:07 +0200

    Code smelak zuzenduta

--- src/main/java/edu/ncsu/monopoly/gui/MainWindow.java
+++ src/main/java/edu/ncsu/monopoly/gui/MainWindow.java
@@ -49,6 +49,7 @@ public class MainWindow extends JFrame implements MonopolyGUI{
 		c.add(westPanel, BorderLayout.WEST);
 		
 		this.addWindowListener(new WindowAdapter(){
+			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
